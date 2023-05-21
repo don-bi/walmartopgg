8b900d765414981350b1479e384a843b896ce6df
don-bi <dbi30@stuy.edu>
2023-05-21 02:43:01 -0400

    added player data

--- app/static/js/match.js
+++ app/static/js/match.js
@@ -3,6 +3,28 @@ playerBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         playerBtns.forEach(element => {element.classList.remove('active')});
         btn.classList.add('active');
-        window.scrollTo(0, 500);
+        window.scrollTo(0, window.innerHeight);
     })
+});
+
+const kdaBoxes = document.querySelectorAll('.kda-box');
+console.log(kdaBoxes)
+kdaBoxes.forEach(box => {
+    box.childNodes.forEach(element => {
+        if (element.innerHTML != undefined ) {
+            element.style.color = 'white';
+        }
+    });
+    const killNode = box.childNodes[1];
+    const kills = killNode.innerHTML.split('')[0];
+    const deathNode = box.childNodes[3]
+    const deaths = deathNode.innerHTML.split('')[0];
+    if (deaths == '0') {
+        killNode.style.color = 'yellow';
+        deathNode.style.color = 'yellow';
+    } else if (deaths > kills) {
+        deathNode.style.color = 'rgb(255, 77, 77)';
+    } else if (deaths < kills) {
+        killNode.style.color = 'rgb(92, 214, 92)';
+    }
 });
