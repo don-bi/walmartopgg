45eecb03da7f3f25e67623d9b2838aa701fb6433
ychan05 <yatlongc2005@gmail.com>
2023-05-23 00:47:28 -0400

    work

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,10 @@ def match(match_id):
     participant_data = db.get_match_participant_data(match_id)
     positions = [['blueTop', 'blueJungle', 'blueMiddle', 'blueBottom', 'blueSupport'], 
                  ['redTop', 'redJungle', 'redMiddle', 'redBottom', 'redSupport']]
+
+    champ_data = [[db.get_champ_data(participant_data[position]['championName']) for position in positions[0]]
+                 ,[db.get_champ_data(participant_data[position]['championName']) for position in positions[1]]]
+    print(champ_data)
     position_names = ['Top', 'Jungle', 'Middle', 'Bottom', 'Support']
     return render_template('match.html',str=str, match_id=match_id, match_data=match_data,
      participant_data=participant_data, positions=positions, position_names=position_names,
